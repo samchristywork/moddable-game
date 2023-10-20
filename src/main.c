8101a16337c1565d5368034dfcd1b28610ce2e3d
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-10-19 21:04:51 -0500

    feat: Add simple Lua integration
    This simply loads a file called "crosshair.lua", which will serve as our
    first "mod" for this game.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
+#include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
 #include <raylib.h>
 
 int main() {
+  lua_State *L = luaL_newstate();
+
+  luaL_openlibs(L);
+
+  if (luaL_dofile(L, "crosshair.lua") != LUA_OK) {
+    fprintf(stderr, "Failed to run script: %s\n", lua_tostring(L, -1));
+    lua_pop(L, 1); // pop error message from the stack
+  }
+
   const int screenWidth = 800;
   const int screenHeight = 450;
 
@@ -14,9 +26,11 @@ int main() {
     ClearBackground(RAYWHITE);
 
     DrawText("Hello, World!", 190, 200, 20, LIGHTGRAY);
+    trigger_event(L, "render", 800, 600);
 
     EndDrawing();
   }
 
   CloseWindow();
+  lua_close(L);
 }
